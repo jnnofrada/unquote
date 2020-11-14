06a53f7cc97aacdbcf95e78f237499736f66a52a
Jeremiah Nofrada <jnnofrada@gmail.com>
2020-11-14 16:42:22 -0500

    apk built

--- app/src/main/java/com/codecademy/unquote/MainActivity.java
+++ app/src/main/java/com/codecademy/unquote/MainActivity.java
@@ -121,4 +121,5 @@ public class MainActivity extends AppCompatActivity {
             return "You got " + totalCorrect + " right out of " + totalQuestions + ". Better luck next time!";
         }
     }
+
 }
